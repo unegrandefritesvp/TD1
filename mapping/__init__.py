4a626a07a4bbaa070f7b851a06436a484d826f74
unegrandefritesvp <54155361+unegrandefritesvp@users.noreply.github.com>
2021-03-12 15:27:17 +0100

    Delete __init__.py

--- mapping/__init__.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from sqlalchemy.ext.declarative import declarative_base
-
-""" base class from which all mapped classes should inherit """
-Base = declarative_base()
-
-
-def generate_id():
-    return str(uuid.uuid4())
